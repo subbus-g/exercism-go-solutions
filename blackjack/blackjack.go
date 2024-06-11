d7de19424aeef34061deecd926d091908df287b7
Subbu G <subbareddyg83@gmail.com>
2024-06-11 22:42:55 +0530

    add solution for blackjack

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -32,12 +32,23 @@ func ParseCard(card string) int {
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	switch {
+	switch cardsSum := ParseCard(card1) + ParseCard(card2); {
 	case card1 == "ace" && card2 == "ace":
 		return "P"
-	case ParseCard(card1)+ParseCard(card2) == 21 && dealerCard != "ace" && ParseCard(dealerCard) != 10:
+	case cardsSum == 21 && dealerCard != "ace" && ParseCard(dealerCard) != 10:
 		return "W"
-
+	case cardsSum == 21 && (dealerCard == "ace" || ParseCard(dealerCard) == 10):
+		return "S"
+	case cardsSum >= 17 && cardsSum <= 20:
+		return "S"
+	case (cardsSum >= 12 && cardsSum <= 16) && ParseCard(dealerCard) < 7:
+		return "S"
+	case (cardsSum >= 12 && cardsSum <= 16) && ParseCard(dealerCard) >= 7:
+		return "H"
+	case cardsSum <= 11:
+		return "H"
+	default:
+		return ""
 	}
-	return "H"
+
 }
