405b827c5e1ed40a50b509dba375e0a20dfa0bdd
subbus <subbareddyg83@gmail.com>
2024-06-21 22:11:12 +0530

    add animal magic

--- animal-magic/animal_magic.go
+++ animal-magic/animal_magic.go
@@ -1,6 +1,8 @@
 package chance
 
-import "math/rand"
+import (
+	"math/rand"
+)
 
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
@@ -9,10 +11,14 @@ func RollADie() int {
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return rand.Float64() * 12
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Shuffle(len(animals), func(i, j int) {
+		animals[i], animals[j] = animals[j], animals[i]
+	})
+	return animals
 }
