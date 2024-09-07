ab24219f457f10527f328658ca5ffd46271e1c8d
sgoluguri <sgoluguri@uptycs.com>
2024-09-07 13:12:41 +0530

    add solution for InterestRate

--- interest-is-interesting/interest_is_interesting.go
+++ interest-is-interesting/interest_is_interesting.go
@@ -2,7 +2,19 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+	// panic("Please implement the InterestRate function")
+	var interestRate float32
+	switch {
+	case balance < 0:
+		interestRate = 3.213
+	case balance >= 0 && balance < 1000:
+		interestRate = 0.5
+	case balance >= 1000 && balance < 5000:
+		interestRate = 1.621
+	case balance >= 5000:
+		interestRate = 2.475
+	}
+	return interestRate
 }
 
 // Interest calculates the interest for the provided balance.
