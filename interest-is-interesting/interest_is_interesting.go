65f2d5e34c1374e2522fc3f3f37543f0f70c8004
sgoluguri <sgoluguri@uptycs.com>
2024-09-07 13:19:40 +0530

    add AnnualBalanceUpdate

--- interest-is-interesting/interest_is_interesting.go
+++ interest-is-interesting/interest_is_interesting.go
@@ -25,7 +25,8 @@ func Interest(balance float64) float64 {
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	// panic("Please implement the AnnualBalanceUpdate function")
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
